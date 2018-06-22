7d75dddb886694316bd1cb2e67c3e1ddc290ffd2
ankitverma22 <40348102+ankitverma22@users.noreply.github.com>
2018-06-23 00:02:12 +0530

    Linked list (#2)
    * addition in linked list
    
    * added stack class
    
    * deleate in link list

--- linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list.py
@@ -30,14 +30,37 @@ class SinglyLinkedList:
 
         temp = self.head
 
-       # for i in range(self.length -1):
-            #temp = temp.next
 
         while temp.next:
             temp = temp.next
 
         temp.next = Node(data, None)
         self.length += 1
+
+    def delete(self, value):
+        if not self.head:
+            return
+
+        temp = self.head
+        # deleting head node
+        if self.head.data is value:
+            self.head = self.head.next
+            temp.next = None
+            temp = None
+            return
+
+        # deleting intermediate node
+        prev = None
+        while temp:
+            if temp.data is value:
+                prev.next = temp.next
+                temp.next = None
+                temp = None
+                break
+
+            prev = temp
+            temp = temp.next
+
 
     def print_list(self):
         temp = self.head
@@ -54,8 +77,18 @@ def main():
     singly_linked_list = SinglyLinkedList()
     for i in range(12):
         singly_linked_list.add_at_beginning(i)
-        singly_linked_list.print_list()
+        # singly_linked_list.print_list()
         print("  ",singly_linked_list.length)
+
+        # singly_linked_list.delete(4)
+        # singly_linked_list.print_list()
+        # singly_linked_list.delete(13)
+        # singly_linked_list.print_list()
+        singly_linked_list.delete(5)
+        singly_linked_list.print_list()
+
+
+
 
     singly_linked_list.print_list()
 
